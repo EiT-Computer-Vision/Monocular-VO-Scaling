c6e16794879309a11e2eddfeec5aea4f56a420f2
KjetilSekseKristiansen <kjetsekse@gmail.com>
2020-03-25 09:07:01 +0100

    scale estimation with image

--- test.py
+++ test.py
@@ -1,51 +1,44 @@
 import numpy as np
 import cv2
-
+from matplotlib import pyplot as plt
 from visual_odometry import PinholeCamera, VisualOdometry
 
+sequence = 1
+height, width = np.shape(cv2.imread('../dataset/sequences/'+ str(sequence).zfill(2) + '/image_0/'+str(0).zfill(6)+'.png', 0))
+cam = PinholeCamera(width, height, 718.8560, 718.8560, 607.1928, 185.2157)
+vo = VisualOdometry(cam, '../dataset/poses/'+str(sequence).zfill(2)+'.txt')
 
-cam = PinholeCamera(1241.0, 376.0, 718.8560, 718.8560, 607.1928, 185.2157)
-vo = VisualOdometry(cam, './dataset/poses/03.txt')
-
-traj = np.zeros((600,600,3), dtype=np.uint8)
+window_width, window_height = 600, 600
+traj = np.zeros((window_width, window_height,3), dtype=np.uint8)
 
 x, y, z = 0., 0., 0.
 true_x1, true_y1, true_z1 = 0., 0., 0.
 vx, vy, vz = 0., 0., 0.
 true_vx, true_vy, true_vz = 0., 0., 0.
-
 for img_id in range(5000): # Terminates on end of image sequence
-	img = cv2.imread('./dataset/sequences/01/image_0/'+str(img_id).zfill(6)+'.png', 0)
+	img = cv2.imread('../dataset/sequences/'+ str(sequence).zfill(2) + '/image_0/'+str(img_id).zfill(6)+'.png', 0)
 	vo.update(img, img_id)
 	cur_t = vo.cur_t
 	Ts = 0.1
 	if(img_id > 2):
 		vx, vy, vz = (cur_t[0] - x) / Ts, (cur_t[1] - y) / Ts, (cur_t[2] - z) / Ts
 		x, y, z = cur_t[0], cur_t[1], cur_t[2]
-	draw_x, draw_y = int(x)+50, int(z)+90
+	offset_x, offset_z = 50, 600
+	draw_x, draw_y = int(x) + offset_x, int(z) + offset_z
 
-	print('true velocity in x is: ', (vo.trueX - true_x1)/Ts)
-	print('estimated velocity in x is: ', vx)
-	print('true velocity in y is: ', (vo.trueY - true_y1) / Ts)
-	print('estimated velocity in y is: ', vy)
-	print('true velocity in z is: ',(vo.trueZ - true_z1) / Ts)
-	print('estimated velocity in z is: ', vz)
-	print('')
 
 	true_x1, true_y1, true_z1 = vo.trueX, vo.trueY, vo.trueZ
-	true_x, true_y = int(true_x1)+50, int(true_z1)+90 # translate to fit to frame
+	true_x, true_y = int(true_x1)+offset_x, int(true_z1) + offset_z # translate to fit to frame
 
-	cv2.circle(traj, (draw_x,draw_y), 1, (img_id*255/4540,255-img_id*255/4540,0), 1)
+	plt_scale = 4540
 	cv2.circle(traj, (true_x,true_y), 1, (0,0,255), 2)
+	cv2.circle(traj, (draw_x,draw_y), 1, (img_id*255/plt_scale,255-img_id*255/plt_scale,0), 1 )
 	cv2.rectangle(traj, (10, 20), (600, 60), (0,0,0), -1)
 	#text = "Coordinates: x=%2fm y=%2fm z=%2fm"%(x, y, z)
 	text = "current velocity: x=%2fm/s y=%2fm/s z=%2fm/s"%(vx, vy, vz)
 	cv2.putText(traj, text, (20,40), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1, 8)
 
 	cv2.imshow('Road facing camera', img)
-	cv2.namedWindow('Trajectory', cv2.WINDOW_NORMAL)
 	cv2.imshow('Trajectory', traj)
-	cv2.resizeWindow('Trajectory', 1000,1000)
-	cv2.waitKey(1)
-
+	cv2.waitKey(10)
 cv2.imwrite('map.png', traj)
--- visual_odometry.py
+++ visual_odometry.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
-
+from rotations import *
+from matplotlib import pyplot as plt
 STAGE_FIRST_FRAME = 0
 STAGE_SECOND_FRAME = 1
 STAGE_DEFAULT_FRAME = 2
@@ -19,6 +20,15 @@ def featureTracking(image_ref, image_cur, px_ref):
 
 	return kp1, kp2
 
+def camera_matrices(R_old, R, t, K):
+	P_0 = np.zeros((3, 4))
+	tilt = 4.6
+	init_R = rotateZ(tilt)[0:3, 0:3]
+	P_0[0:3, 0:3] = init_R
+	P_1 = np.zeros((3, 4))
+	P_1[0:3, 0:3] = R
+	P_1[:, 3] = np.squeeze(t)
+	return K @ P_0, K @ P_1
 
 class PinholeCamera:
 	def __init__(self, width, height, fx, fy, cx, cy,
@@ -43,8 +53,12 @@ class VisualOdometry:
 		self.cur_t = None
 		self.px_ref = None
 		self.px_cur = None
+		self.px_planar_ref = None
+		self.px_planar_cur = None
+		self.scale = 1
 		self.focal = cam.fx
 		self.pp = (cam.cx, cam.cy)
+		self.k = np.array([[self.focal, 0, cam.cx], [0, self.focal, cam.cy], [0, 0, 1]])
 		self.trueX, self.trueY, self.trueZ = 0, 0, 0
 		self.detector = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
 		with open(annotations) as f:
@@ -79,8 +93,32 @@ class VisualOdometry:
 		E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC, prob=0.999, threshold=1.0)
 		_, R, t, mask = cv2.recoverPose(E, self.px_cur, self.px_ref, focal=self.focal, pp = self.pp)
 		absolute_scale = self.getAbsoluteScale(frame_id)
+
+		self.track_window(self.last_frame, self.new_frame)
+		P0, P1 = camera_matrices(self.cur_R, R, t, self.k)
+		points = cv2.triangulatePoints(P0, P1, self.px_planar_ref.T, self.px_planar_cur.T)
+		points = points.T
+		reproj_pix = np.zeros_like(points)
+		for i in range(len(points)):
+			reproj_pix[i,0:3] = -P1 @ points[i]
+			points[i] /= points[i,3]
+			print(points[i])
+		y_mean = np.mean(abs(points[:,1]))
+		scale = 1.65/y_mean
+		alpha = 0.5
+		offset = 0
+		if(abs(np.amax(abs(points[:,1]) - y_mean))) > 0.5 or y_mean < 1:
+			self.scale = self.scale
+		else:
+			self.scale = (1-alpha)*self.scale + alpha*scale + offset
+		print(absolute_scale)
+		print(self.scale)
+		print('')
+		#print(reproj_pix)
+		#print(self.px_planar_cur)
+
 		if(absolute_scale > 0.1):
-			self.cur_t = self.cur_t + absolute_scale*self.cur_R.dot(t)
+			self.cur_t = self.cur_t + self.scale*self.cur_R.dot(t)
 			self.cur_R = R.dot(self.cur_R)
 		if(self.px_ref.shape[0] < kMinNumFeature):
 			self.px_cur = self.detector.detect(self.new_frame)
@@ -97,3 +135,34 @@ class VisualOdometry:
 		elif(self.frame_stage == STAGE_FIRST_FRAME):
 			self.processFirstFrame()
 		self.last_frame = self.new_frame
+
+	def track_window(self, im1, im2):
+		"""Made as part of EiT computer vision village project"""
+		FEATURE_PARAMS = dict(maxCorners=10,qualityLevel=0.001,minDistance=20,blockSize=7)
+		LK_PARAMS = dict(winSize=(21, 21), maxLevel = 3, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
+
+		crop_x_lower, crop_x_upper = 250, 320
+		crop_y_lower, crop_y_upper = 520, 580
+		crop_img1 = im1[crop_x_lower:crop_x_upper, crop_y_lower:crop_y_upper]
+
+		p0 = cv2.goodFeaturesToTrack(crop_img1, **FEATURE_PARAMS)
+		p0 = np.squeeze(p0)
+		for i in p0:
+			i[0] += crop_y_lower
+			i[1] += crop_x_lower
+		# Calculate optical flow
+		p1, st, err = cv2.calcOpticalFlowPyrLK(im1, im2, p0, None, **LK_PARAMS)
+		print('amount of tracked points are :', len(p1))
+		for i in p1:
+			i[0] = round(i[0])
+			i[1] = round(i[1])
+			self.px_planar_ref, self.px_planar_cur = np.squeeze(p0), np.squeeze(p1)
+
+	def plot_correspondences(self):
+		plt.subplot(2, 1, 1)
+		plt.imshow(self.last_frame)
+		plt.scatter(self.px_planar_ref[:, 0], self.px_planar_ref[:, 1], c='red')
+		plt.subplot(2, 1, 2)
+		plt.imshow(self.new_frame)
+		plt.scatter(self.px_planar_cur[:, 0], self.px_planar_cur[:, 1], c='red')
+		plt.show()
